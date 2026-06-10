03d17fa1157ac2c885b560f85cec86e393518b66
wwoosshh <s01055310644@gmail.com>
2026-06-10 17:28:38 +0900

    fix: close nest app after each e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -16,6 +16,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterEach(async () => {
+    await app.close();
+  });
+
   it('/health (GET)', () => {
     return request(app.getHttpServer())
       .get('/health')
